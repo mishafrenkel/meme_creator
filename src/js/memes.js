0cabe48e765fd3ff2fd9ff4b4d5ce67c90697987
Michael Frenkel <michael.frenkel@outlook.com>
2019-02-16 21:52:04 -0800

    Added FileReader logic

--- src/js/memes.js
+++ src/js/memes.js
@@ -13,7 +13,6 @@ class Memes {
   
         this.createCanvas();
         this.addEventListeners();
-        alert('init');
     }
 
     createCanvas() {
@@ -23,13 +22,21 @@ class Memes {
         this.$canvas.height = canvasHeight;
         this.$canvas.width = canvasWidth;
     }
-
     createMeme() {
        let context = this.$canvas.getContext('2d');
+       if (this.$imageInput.files && this.$imageInput.files[0]) {
+           let reader = new FileReader();
+           
+           reader.onload = () => {
+               console.log('file completely read');
+           };
+           reader.readAsDataURL(this.$imageInput.files[0]);
+           console.log('This will get printed first!');
+       }
     }
 
     addEventListeners() {
-        // this.createMeme = this.createMeme.bind(this);
+        this.createMeme = this.createMeme.bind(this);
         // this.downloadMeme = this.downloadMeme.bind(this);
         let inputNodes = [this.$topTextInput, this.$bottomTextInput, this.$imageInput];
     
